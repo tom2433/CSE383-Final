e71ee3e77f00ac78c2f5258c1f7b5a8d9d74d07e
tom2433 <tom.2433.england@gmail.com>
2025-11-05 23:49:25 +0000

    Initial file upload for docker, apache, SQLite database, phpliteadmin, and password protection

---
+++ build-ssl.sh
@@ -0,0 +1,1 @@
+openssl req -x509 -new -out mycert.crt -keyout mycert.key -days 365 -newkey rsa:4096 -sha256 -nodes
---
+++ html/final.class.php
@@ -0,0 +1,175 @@
+<?php 
+class final_rest
+{
+
+
+
+/**
+ * @api  /api/v1/setTemp/
+ * @apiName setTemp
+ * @apiDescription Add remote temperature measurement
+ *
+ * @apiParam {string} location
+ * @apiParam {String} sensor
+ * @apiParam {double} value
+ *
+ * @apiSuccess {Integer} status
+ * @apiSuccess {string} message
+ *
+ * @apiSuccessExample Success-Response:
+ *     HTTP/1.1 200 OK
+ *     {
+ *              "status":0,
+ *              "message": ""
+ *     }
+ *
+ * @apiError Invalid data types
+ *
+ * @apiErrorExample Error-Response:
+ *     HTTP/1.1 200 OK
+ *     {
+ *              "status":1,
+ *              "message":"Error Message"
+ *     }
+ *
+ */
+	public static function setTemp ($location, $sensor, $value)
+
+	{
+		if (!is_numeric($value)) {
+			$retData["status"]=1;
+			$retData["message"]="'$value' is not numeric";
+		}
+		else {
+			try {
+				EXEC_SQL("insert into temperature (location, sensor, value, date) values (?,?,?,CURRENT_TIMESTAMP)",$location, $sensor, $value);
+				$retData["status"]=0;
+				$retData["message"]="insert of '$value' for location: '$location' and sensor '$sensor' accepted";
+			}
+			catch  (Exception $e) {
+				$retData["status"]=1;
+				$retData["message"]=$e->getMessage();
+			}
+		}
+
+		return json_encode ($retData);
+	}
+
+
+/**
+ * @api  /api/v1/getLevel/
+ * @apiName getLevel
+ * @apiDescription Return all level data from database
+ *
+ * @apiSuccess {Integer} status
+ * @apiSuccess {string} message
+ *
+ * @apiSuccessExample Success-Response:
+ *     HTTP/1.1 200 OK
+ *     {
+ *              "status":0,
+ *              "message": ""
+ *              "result": [
+ *                { 
+ *                  levelID: 1,
+ *                  description: "",
+ *                  prompt: ""
+ *              ]
+ *     }
+ *
+ * @apiError Invalid data types
+ *
+ * @apiErrorExample Error-Response:
+ *     HTTP/1.1 200 OK
+ *     {
+ *              "status":1,
+ *              "message":"Error Message"
+ *     }
+ *
+ */
+  public static function getLevel () {
+		return json_encode ($retData);
+  }
+
+/**
+ * @api  /api/v1/addLog/
+ * @apiName addLog
+ * @apiDescription Add record to logfile
+ *
+ * @apiParam {string} level
+ * @apiParam {String} systemPrompt
+ * @apiParam {String} userPrompt
+ * @apiParam {string} chatResponse
+ * @apiParam {Integer} inputTokens
+ * @apiParam {Integer} outputTokens
+ *
+ * @apiSuccess {Integer} status
+ * @apiSuccess {string} message
+ *
+ * @apiSuccessExample Success-Response:
+ *     HTTP/1.1 200 OK
+ *     {
+ *              "status":0,
+ *              "message": ""
+ *     }
+ *
+ * @apiError Invalid data types
+ *
+ * @apiErrorExample Error-Response:
+ *     HTTP/1.1 200 OK
+ *     {
+ *              "status":1,
+ *              "message":"Error Message"
+ *     }
+ *
+ */
+  public static function addLog ($level, $systemPrompt, $userPrompt, $chatResponse, $inputTokens, $outputTokens) {
+		return json_encode ($retData);
+  }
+  
+
+/**
+ * @api  /api/v1/getLog/
+ * @apiName getLog
+ * @apiDescription Retrieve Log Records
+ *
+ * @apiParam {string} date
+ * @apiParam {String} numRecords
+ *
+ * @apiSuccess {Integer} status
+ * @apiSuccess {string} message
+ *
+ * @apiSuccessExample Success-Response:
+ *     HTTP/1.1 200 OK
+ *     {
+ *              "status":0,
+ *              "message": ""
+ *              "result": [
+ *                { 
+ *                  timeStamp: "YYYY-MM-DD HH:MM:SS",
+ *                  level: "",
+ *                  systemPrompt: "",
+ *                  userPrompt: "",
+ *                  chatResponse: "",
+ *                  inputTokens: 0,
+ *                  outputTokens: 0
+ *              ]
+ *     }
+ *
+ * @apiError Invalid data types
+ *
+ * @apiErrorExample Error-Response:
+ *     HTTP/1.1 200 OK
+ *     {
+ *              "status":1,
+ *              "message":"Error Message"
+ *     }
+ *
+ */
+  public static function getLog ($date, $numRecords) {
+		return json_encode ($retData);
+  }
+  
+
+}
+
